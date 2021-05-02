fad858e8f8724cb4f1fe4a08d1080356e2263546
jeevana <jeevana@wipro.com>
2021-05-03 03:33:04 +0530

    dev developed code for homeloanlimit hdfc

---
+++ src/main/java/hdfc/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package hdfc.repository;
+
+import hdfc.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+}
